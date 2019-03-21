1a4b32bd3d3df77d08c4d11058f422d4db09c057
YobraulyCruz <43036792+YobraulyCruz@users.noreply.github.com>
2019-03-21 08:56:24 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,29 @@
+$(".too").click(function(){
+    var a = $(".lul").val();
+    $("#oof").html(a);
+});
+var spego;
+$(".wave").click(function(){
+    $("#q").show();
+});
+
+var spoon;
+$(".moon").click(function(){
+    spoon = $(".water").val();
+    $(".end").html(spoon);
+    $(".end").html(parseInt(spoon)*365);
+});
+$(".moon").click(function(){
+    $(".po").show();
+});
+
+var nub;
+$(".drip").click(function(){
+    nub = $(".air").val();
+    $(".mum").html(nub);
+    $(".mum").html(parseInt(nub)*53);
+});
+$(".drip").click(function(){
+    $(".oof").show();
+});
+   
